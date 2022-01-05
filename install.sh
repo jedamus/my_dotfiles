9933adfa4df3df4d9432f79a5498f6b225e4041c
Leander Jedamus <ljedamus@web.de>
2022-01-05 18:11:36 +0100

    changed cp command

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
 # erzeugt Montag, 14. Dezember 2020 14:26 (C) 2020 von Leander Jedamus
+# modifiziert Mittwoch, 05. Januar 2022 18:02 von Leander Jedamus
 # modifiziert Dienstag, 29. Dezember 2020 15:39 von Leander Jedamus
 # modifiziert Montag, 14. Dezember 2020 14:55 von Leander Jedamus
 
 cd files
-cp -Rvp .[a-zA-Z0-9_]* $HOME
+cp -Rvp .[^.]* $HOME
 cd ..
 
 chmod 700 $HOME/.ssh
