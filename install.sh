8b153e74327028719964e72e814af478b4148694
Leander Jedamus <ljedamus@web.de>
2022-03-17 12:19:42 +0100

    changed to not overwrite authorzed_keys, when already there

--- install.sh
+++ install.sh
@@ -5,12 +5,28 @@
 # modifiziert Dienstag, 29. Dezember 2020 15:39 von Leander Jedamus
 # modifiziert Montag, 14. Dezember 2020 14:55 von Leander Jedamus
 
+## HOME=$HOME/repositories
+## mkdir -p $HOME
+
+ak=$HOME/.ssh/authorized_keys
+tak=/tmp/authorized_keys
+
+if [ -f $ak ]; then
+  no_replace=TRUE
+  cp -p $ak $tak
+fi
+
 cd files
 cp -Rvp .[^.]* $HOME
 cd ..
 
+
 chmod 700 $HOME/.ssh
-chmod 600 $HOME/.ssh/authorized_keys
+chmod 600 $ak
+
+if [ ! -z $no_replace ]; then
+  mv $tak $ak
+fi
 
 # vim:ai sw=2
 
